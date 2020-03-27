94e3cceca4d5596c0e09f7b1feba4b21fcdadd8f
ajaydy <mohdjamilafiq@gmail.com>
2020-03-27 19:01:23 +0800

    school-API

--- routers/handlers.go
+++ routers/handlers.go
@@ -5,8 +5,6 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"school/helpers"
-	"school/middleware"
-	"school/session"
 )
 
 type (
@@ -40,30 +38,42 @@ func InitHandlers() *mux.Router {
 
 	apiV1 := r.PathPrefix("/api/v1").Subrouter()
 
+	apiV1.Handle("/student/{id}", HandlerFunc(HandlerStudentDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/lecturer/{id}", HandlerFunc(HandlerLecturerDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/subjects/{id}", HandlerFunc(HandlerSubjectDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/attendance/{id}", HandlerFunc(HandlerAttendanceDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/classroom/{id}", HandlerFunc(HandlerClassroomDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/faculty/{id}", HandlerFunc(HandlerFacultyDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/intake/{id}", HandlerFunc(HandlerIntakeDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/program/{id}", HandlerFunc(HandlerProgramDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/result/{id}", HandlerFunc(HandlerResultDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/session/{id}", HandlerFunc(HandlerSessionDetail)).Methods(http.MethodGet)
+	apiV1.Handle("/studentEnroll/{id}", HandlerFunc(HandlerStudentEnrollDetail)).Methods(http.MethodGet)
+
 	//apiV1.Use(middleware.BasicAuthMiddleware)
 
-	apiV1.Handle("/students", HandlerFunc(HandlerStudents)).Methods(http.MethodGet)
-	apiV1.Handle("/students/{id}", middleware.SessionMiddleware(middleware.RolesMiddleware(
-		HandlerFunc(HandlerStudentsDetail), session.STUDENT_ROLE, session.ADMIN_ROLE))).Methods(http.MethodGet)
-	apiV1.Handle("/students", HandlerFunc(HandlerAddStudents)).Methods(http.MethodPost)
-	apiV1.Handle("/students/{id}", HandlerFunc(HandlerUpdateStudents)).Methods(http.MethodPut)
-	apiV1.Handle("/students/register", HandlerFunc(HandlerRegisterStudents)).Methods(http.MethodPost)
-	apiV1.Handle("/students/login", HandlerFunc(HandlerLoginStudents)).Methods(http.MethodPost)
-
-	apiV1.Handle("/lecturers", HandlerFunc(HandlerLecturers)).Methods(http.MethodGet)
-	apiV1.Handle("/lecturers/{id}", HandlerFunc(HandlerLecturersDetail)).Methods(http.MethodGet)
-	apiV1.Handle("/lecturers", HandlerFunc(HandlerAddLecturers)).Methods(http.MethodPost)
-	apiV1.Handle("/lecturers/{id}", HandlerFunc(HandlerUpdateLecturers)).Methods(http.MethodPut)
-
-	apiV1.Handle("/subjects", HandlerFunc(HandlerSubjects)).Methods(http.MethodGet)
-	apiV1.Handle("/subjects/{id}", HandlerFunc(HandlerSubjectsDetail)).Methods(http.MethodGet)
-	apiV1.Handle("/subjects", HandlerFunc(HandlerAddSubjects)).Methods(http.MethodPost)
-	apiV1.Handle("/subjects/{id}", HandlerFunc(HandlerUpdateSubjects)).Methods(http.MethodPut)
-
-	apiV1.Handle("/score", HandlerFunc(HandlerStudentsWithScore)).Methods(http.MethodGet)
-	apiV1.Handle("/score/{id}", HandlerFunc(HandlerStudentsWithScoreDetail)).Methods(http.MethodGet)
-
-	apiV1.Handle("/enroll", HandlerFunc(HandlerAddStudentEnroll)).Methods(http.MethodPost)
+	//apiV1.Handle("/students", HandlerFunc(HandlerStudents)).Methods(http.MethodGet)
+	//apiV1.Handle("/students/{id}", middleware.SessionMiddleware(middleware.RolesMiddleware(
+	//	HandlerFunc(HandlerStudentsDetail), session.STUDENT_ROLE, session.ADMIN_ROLE))).Methods(http.MethodGet)
+	//apiV1.Handle("/students", HandlerFunc(HandlerAddStudents)).Methods(http.MethodPost)
+	//apiV1.Handle("/students/{id}", HandlerFunc(HandlerUpdateStudents)).Methods(http.MethodPut)
+	//apiV1.Handle("/students/register", HandlerFunc(HandlerRegisterStudents)).Methods(http.MethodPost)
+	//apiV1.Handle("/students/login", HandlerFunc(HandlerLoginStudents)).Methods(http.MethodPost)
+	//
+	//apiV1.Handle("/lecturers", HandlerFunc(HandlerLecturers)).Methods(http.MethodGet)
+	//apiV1.Handle("/lecturers/{id}", HandlerFunc(HandlerLecturersDetail)).Methods(http.MethodGet)
+	//apiV1.Handle("/lecturers", HandlerFunc(HandlerAddLecturers)).Methods(http.MethodPost)
+	//apiV1.Handle("/lecturers/{id}", HandlerFunc(HandlerUpdateLecturers)).Methods(http.MethodPut)
+	//
+	//apiV1.Handle("/subjects", HandlerFunc(HandlerSubjects)).Methods(http.MethodGet)
+	//apiV1.Handle("/subjects/{id}", HandlerFunc(HandlerSubjectsDetail)).Methods(http.MethodGet)
+	//apiV1.Handle("/subjects", HandlerFunc(HandlerAddSubjects)).Methods(http.MethodPost)
+	//apiV1.Handle("/subjects/{id}", HandlerFunc(HandlerUpdateSubjects)).Methods(http.MethodPut)
+	//
+	//apiV1.Handle("/score", HandlerFunc(HandlerStudentsWithScore)).Methods(http.MethodGet)
+	//apiV1.Handle("/score/{id}", HandlerFunc(HandlerStudentsWithScoreDetail)).Methods(http.MethodGet)
+	//
+	//apiV1.Handle("/enroll", HandlerFunc(HandlerAddStudentEnroll)).Methods(http.MethodPost)
 
 	return r
 }
